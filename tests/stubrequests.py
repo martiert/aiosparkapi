704f8626e7e5670d3ac98f38e3e95783ba6b4241
Martin Ertsaas <martiert@gmail.com>
2017-09-17 12:25:07 +0200

    Move StubRequests out

---
+++ tests/stubrequests.py
@@ -0,0 +1,29 @@
+class StubRequests:
+
+    def __init__(self):
+        self.path = None
+        self.list_parameters = None
+        self.create_parameters = None
+        self.get_id = None
+        self.delete_id = None
+        self.results = []
+
+    async def list(self, path, parameters):
+        self.path = path
+        self.list_parameters = parameters
+        return self.results
+
+    async def create(self, path, parameters):
+        self.path = path
+        self.create_parameters = parameters
+        return self.results
+
+    async def get(self, path, get_id):
+        self.path = path
+        self.get_id = get_id
+        return self.results
+
+    async def delete(self, path, delete_id):
+        self.path = path
+        self.delete_id = delete_id
+        return self.results
